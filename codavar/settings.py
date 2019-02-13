8e38c475dde22519286a8f4e5603e1176fffd1da
Sajad Hashemian <sajadhsm@gmail.com>
2019-02-13 22:47:58 +0330

    Restructure static files

--- codavar/settings.py
+++ codavar/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'codavar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,10 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/root/')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "static/files/"),
 ]
 
 # Uploaded files
