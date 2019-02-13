60d6e7017f4e47561d0dd042691327a836b92391
Sajad Hashemian <sajadhsm@gmail.com>
2019-02-14 01:04:59 +0330

    Add respective background color for sub cells in leaderboard

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 def timedelta_hh_mm(time):
     seconds = round(time.total_seconds())
     return str(timedelta(seconds=seconds))
+
+@register.filter
+def respective_bg_color_css_rule(sub):
+    '''Give leaderboard submission cell background color'''
+    opacity = sub.judge_score / sub.problem.score
+    return 'background-color: rgba(41, 168, 71, {});'.format(opacity)
