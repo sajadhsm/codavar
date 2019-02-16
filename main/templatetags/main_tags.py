f64186d1c71ed1c1aeb6e80d2c9ad3997b50761e
Sajad Hashemian <sajadhsm@gmail.com>
2019-02-16 18:55:26 +0330

    Hide timedelta seconds for display

--- main/templatetags/main_tags.py
+++ main/templatetags/main_tags.py
@@ -5,9 +5,8 @@ from django import template
 register = template.Library()
 
 @register.filter
-def timedelta_hh_mm(time):
-    seconds = round(time.total_seconds())
-    return str(timedelta(seconds=seconds))
+def timedelta_hide_seconds(time):
+    return ':'.join(str(time).split(':')[:2])
 
 @register.filter
 def respective_bg_color_css_rule(sub):
