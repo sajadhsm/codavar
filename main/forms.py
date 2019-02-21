2d3413bde51d5751b27c62ad895cea844a670e70
Sajad Hashemian <sajadhsm@gmail.com>
2019-02-21 18:14:57 +0330

    Restrict submission zip_file upload size

--- main/forms.py
+++ main/forms.py
@@ -5,12 +5,18 @@ from django.core.exceptions import ValidationError
 
 from .models import Submission
 
+SUBMISSION_ZIP_MAX_SIZE = 2097152 #2MB
+
 class SubmissionForm(ModelForm):
     def clean_zip_file(self):
         file = self.cleaned_data.get("zip_file")
         filetype = magic.from_buffer(file.read(), mime=True)
+        
         if not "application/zip" in filetype:
             raise ValidationError('File is not ZIP.', code='invalid')
+        
+        if file.size > SUBMISSION_ZIP_MAX_SIZE:
+            raise ValidationError('File size is greater than 2MB.', code='invalid')
         return file
 
     class Meta:
