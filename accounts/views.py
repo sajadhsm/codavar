66f1969e5dd68f5437b53a6655f1e62399ac4430
Sajad Hashemian <sajadhsm@gmail.com>
2019-03-19 19:00:26 +0330

    Add change password page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.decorators import login_required
 
 from .models import CustomUser
 from .forms import CustomUserCreationForm, CustomUserEditForm
@@ -17,6 +20,7 @@ def signup(request):
         
     return render(request, 'accounts/signup.html', {'form': form})
 
+@login_required
 def edit_user_view(request):
     if request.method == 'POST':
         form = CustomUserEditForm(request.POST, instance=request.user)
@@ -24,7 +28,25 @@ def edit_user_view(request):
             form.save()
             messages.success(request, 'Your information were updated successfully!')
             return redirect('account_edit')
+        else:
+            messages.error(request, 'Please correct the error(s) below.')
     else:
         form = CustomUserEditForm(instance=request.user)
     
     return render(request, 'accounts/edit.html', {'form': form})
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error(s) below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'accounts/change_password.html', {'form': form})
+    
