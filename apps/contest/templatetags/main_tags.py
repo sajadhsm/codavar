383ccb52f5ca020372fd20fda09e317bcf53c935
Sajad Hashemian <sajadhsm@gmail.com>
2019-03-27 22:09:59 +0430

    Submission background color opacity base on its relative_score

--- apps/contest/templatetags/main_tags.py
+++ apps/contest/templatetags/main_tags.py
@@ -7,9 +7,3 @@ register = template.Library()
 @register.filter
 def timedelta_hide_seconds(time):
     return ':'.join(str(time).split(':')[:2])
-
-@register.filter
-def respective_bg_color_css_rule(sub):
-    '''Give leaderboard submission cell background color'''
-    opacity = sub.judge_score / sub.problem.score
-    return 'background-color: rgba(41, 168, 71, {});'.format(opacity)
