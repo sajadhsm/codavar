6a1bd093a247775af12f537be95e0ed8f1d9e656
Sajad Hashemian <sajadhsm@gmail.com>
2019-05-02 12:19:11 +0430

    Improve FrontEndContestParticipation admin

--- apps/contest/admin.py
+++ apps/contest/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models.front_end_contest import FrontEndContest, FrontEndContestParticipation
 
+@admin.register(FrontEndContestParticipation)
+class FrontEndContestParticipationAdmin(admin.ModelAdmin):
+    readonly_fields = ('reg_date',)
+    
+    list_display = ['participant', 'contest', 'reg_date']
+    list_filter = ['contest', 'reg_date']
+    search_fields = ['contest', 'participant']
+
 admin.site.register(FrontEndContest)
-admin.site.register(FrontEndContestParticipation)
