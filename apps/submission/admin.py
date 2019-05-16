924645c8b8c1512f0abd0fe2a77c523b2ce41a9b
Sajad Hashemian <sajadhsm@gmail.com>
2019-05-16 11:54:52 +0430

    Add submission re-judge action

--- apps/submission/admin.py
+++ apps/submission/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models.front_end_submission import FrontEndSubmission
+from apps.contest.tasks import run_selenium_test
 
 @admin.register(FrontEndSubmission)
 class FrontEndSubmissionAdmin(admin.ModelAdmin):
@@ -9,3 +10,13 @@ class FrontEndSubmissionAdmin(admin.ModelAdmin):
     list_display = ['user', 'problem', 'upload_date', 'status']
     list_filter = ['status', 'user', 'problem']
     search_fields = ['user__email', 'problem__title']
+
+    actions = ['rejudge_submissions']
+
+    def rejudge_submissions(self, request, queryset):
+        for submission in queryset:
+            run_selenium_test.delay(submission.pk)
+        sub_count = queryset.count()
+        msg_bit = '1 submission is' if sub_count == 1 else f'{sub_count} submissions are'
+        self.message_user(request, f'{msg_bit} being re-judged. (Reload the page after a few seconds)')
+    rejudge_submissions.short_description = 'Re-judge selected submissions'
